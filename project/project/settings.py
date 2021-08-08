b8f580b913c0f0868c163ecd0e4c5e3e9c6f27e3
Yusdanis Feus Pérez <feus2006@gmail.com>
2021-08-08 09:06:04 -0500

    Added linter to pipeline
     Changes to be committed:
    	new file:   .flake8
    	new file:   .github/workflows/pipeline.yml
    	modified:   project/app/admin.py
    	modified:   project/app/models.py
    	modified:   project/app/tests.py
    	modified:   project/app/views.py
    	modified:   project/project/settings.py
    	modified:   requirements.txt

--- project/project/settings.py
+++ project/project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-uep2k!43%$kl4t^=a=r5&!h9r9s&**p$)qd+l%q^xeupbuokly'
+SECRET_KEY = 'uep2k!43%$kl4t^=a=r5&!h9r9s&**p$)qd+l%q^xeupbuokly'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -86,16 +86,16 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',  # noqa: E501
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',  # noqa: E501
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',  # noqa: E501
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',  # noqa: E501
     },
 ]
 
